0f0f763b9963c961e33b6c8775fd49467bc2635b
neswill1 <nenadkonatar@live.ac.me>
2015-03-26 14:11:58 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+$adresa="141 10th Street South Naples, FL 34103"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad="Naples";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+$telephone=18005289650;// Broj koji se poziva na dugme CALL US
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
